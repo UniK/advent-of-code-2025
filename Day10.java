d6ce46193b8640c8500d11443d47d08896f13675
UniK <unik@users.noreply.github.com>
2025-12-01 10:15:58 +0100

    set up initial project structure for Advent of Code 2025 with Java 25 compact source boilerplate and placeholder files for all 12 days.

---
+++ Day10.java
@@ -0,0 +1,15 @@
+void main() {
+    var lines = Files.readAllLines(Path.of("Day10.input"));
+    IO.println("Part 1: " + part1(lines));
+    IO.println("Part 2: " + part2(lines));
+}
+
+static String part1(var lines) {
+    // TODO
+    return "Not implemented";
+}
+
+static String part2(var lines) {
+    // TODO
+    return "Not implemented";
+}
