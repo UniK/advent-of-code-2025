e8e9450c098065effe0c97d98eb93f543a384809
UniK <unik@users.noreply.github.com>
2025-12-01 15:18:59 +0100

    apply day 1 template to days 2 through 12

--- Day07.java
+++ Day07.java
@@ -1,15 +1,51 @@
-void main() {
-    var lines = Files.readAllLines(Path.of("Day07.input"));
-    IO.println("Part 1: " + part1(lines));
-    IO.println("Part 2: " + part2(lines));
+import static java.lang.IO.println;
+import static java.lang.System.err;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Advent of Code Day 07 solution using Java 25.
+ */
+void main(String... args) {
+    var input = args.length > 0 ? args[0] : "Day07.test";
+
+    try (Stream<String> lineStream = Files.lines(Path.of(input))) {
+        List<String> lines = lineStream.toList();
+        println("""
+                Part 1: %s
+                Part 2: %s
+                """.formatted(
+                solve(1, lines),
+                solve(2, lines)));
+    } catch (IOException e) {
+        err.println("Unable to read input file '" + input + "': " + e.getMessage());
+    }
 }
 
-static String part1(List<String> lines) {
-    // TODO
-    return "Not implemented";
+sealed interface Part permits Part1, Part2 {
+    String compute(List<String> lines);
 }
 
-static String part2(List<String> lines) {
-    // TODO
-    return "Not implemented";
+record Part1() implements Part {
+    public String compute(List<String> lines) {
+        return "Not implemented";
+    }
+}
+
+record Part2() implements Part {
+    public String compute(List<String> lines) {
+        return "Not implemented";
+    }
+}
+
+static String solve(int part, List<String> lines) {
+    return switch (part) {
+        case 1 -> new Part1().compute(lines);
+        case 2 -> new Part2().compute(lines);
+        default -> throw new IllegalArgumentException("Invalid part: " + part);
+    };
 }
